b2061404d7813d9146168dea4e4b979c0dbba362
weird-frog <den.6667531@gmail.com>
2021-04-06 14:57:18 +0300

    Add project files.

---
+++ PR6/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            string path = Path.Combine(Environment.CurrentDirectory, "text.txt"); //путь до папки с exe
+            try
+            {
+                FileStream fileStream = new FileStream(path, FileMode.Create, FileAccess.ReadWrite);
+                byte[] array = new byte[10];
+
+                for (byte i = 0; i < 10; ++i)
+                {
+                    array[i] = (byte)(random.Next(0,255));
+                }
+
+                fileStream.Write(array, 0, 10);
+                int x, count = 0;
+                fileStream.Seek(0, SeekOrigin.Begin);
+
+                for (int i = 0; i < 10; i++)
+                {
+                    x = fileStream.ReadByte();
+                    if (x % 2 != 0)
+                    {
+                        count++;
+                    }
+                    Console.Write(x + " "); //контрольная печать считанного значения
+                }
+                Console.WriteLine($"\nКоличество нечетных: {count}");
+                fileStream.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка при работе с файлом: " + e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PR6_Characters/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR6_Characters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.Combine(Environment.CurrentDirectory, "text1.txt"); //путь до папки с exe
+            try
+
+            {
+
+                // работа с потоком байтов символы
+
+                FileStream fchar = new FileStream(path, FileMode.Create, FileAccess.ReadWrite);
+                char[] x = new char[10];
+                Console.WriteLine("Введите 10 символов");
+                //код из лекции, но почему то i увеличивается на 3 при каджой итерации
+                for (int i = 0; i < 10; ++i)
+                {
+                    x[i] = (char)Console.Read();
+                    fchar.WriteByte((byte)x[i]); // записывается элемент массива
+                }
+                Console.ReadLine();
+                int a, counter = 0;
+                fchar.Seek(0, SeekOrigin.Begin); // текущий указатель - на начало
+
+                for (int i = 0; i < 10; i++)
+                {
+                    a = fchar.ReadByte();
+                    if (a == 90)
+                    {
+                        counter++;
+                    }
+                }
+                Console.WriteLine($"\nСимвол 'Z' встречается {counter} раз(а)");
+                
+                fchar.Close();
+            }
+
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Ошибка работы с файлом: " + e.Message);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
